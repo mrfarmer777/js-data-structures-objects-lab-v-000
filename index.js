3af547a5120829986da7af83097d94291233b1cd
Matt Farmer <mrfarmer777@gmail.com>
2018-07-26 19:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const driver={name:"Larry"};
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  const newObj={};
-  newObj.assign(driver, {key: value});
+  const newObj= Object.assign(driver, {key: value});
   return newObj;
 }
