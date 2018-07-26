68fb9a80612b4f18c99a6369496bd58c2db05332
Matt Farmer <mrfarmer777@gmail.com>
2018-07-26 19:36:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
-  return Object.assign(driver,{key:value});
+  return driver[key]=value;
   
 }
